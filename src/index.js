fd83cc72dbc0f723d1a0e51d8463120c4080abfc
Samse <samuelberisha@gmail.com>
2019-09-27 18:03:11 +0200

    Lesson 77: React Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<p>Test</p>, document.getElementById("root"));
+const ExpenseDashboardPage = () => <div>This ist a test</div>;
+const AddExpensePage = () => <div>This ist a 2</div>;
+const EditExpensePage = () => <div>This ist a 3</div>;
+const HelpPage = () => <div>This ist a 4</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
