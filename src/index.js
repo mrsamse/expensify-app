20b5d44f0edb9269ab59fce876aeca6cf9b2ac15
Samse <samuelberisha@gmail.com>
2019-09-28 10:19:49 +0200

    79. Linking Between Routes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
 const ExpenseDashboardPage = () => <div>This ist a test</div>;
 const AddExpensePage = () => <div>This ist a 2</div>;
 const EditExpensePage = () => <div>This ist a 3</div>;
 const HelpPage = () => <div>This ist a 4</div>;
-const NotFoundPage = () => <div>404</div>;
+const NotFoundPage = () => (
+  <div>
+    404 - <Link to="/">Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>
+      Dashboard
+    </NavLink>
+    <NavLink to="/create" activeClassName="is-active">
+      Create Expense
+    </NavLink>
+    <NavLink to="/edit" activeClassName="is-active">
+      Edit Expense
+    </NavLink>
+    <NavLink to="/help" activeClassName="is-active">
+      Help
+    </NavLink>
+  </header>
+);
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
-      <Route path="/help" component={NotFoundPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
