3e193e4761cf6cbf556c53fd95fd230547c9c9d8
Samse <samuelberisha@gmail.com>
2019-09-28 09:58:37 +0200

    Lesson 78. Setting up a 404

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 const ExpenseDashboardPage = () => <div>This ist a test</div>;
 const AddExpensePage = () => <div>This ist a 2</div>;
 const EditExpensePage = () => <div>This ist a 3</div>;
 const HelpPage = () => <div>This ist a 4</div>;
+const NotFoundPage = () => <div>404</div>;
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
-    </div>
+      <Route path="/help" component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
