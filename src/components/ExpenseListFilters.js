93d19355ee2b0d267503365728ad513d7a3f5625
Samse <samuelberisha@gmail.com>
2019-11-09 12:42:01 +0100

    104. Dropdown for Picking SortBy

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { setTextFilter } from "../actions/filters";
+import { setTextFilter, sortByDate, sortByAmount } from "../actions/filters";
 
 const ExpenseListFilters = ({ filters, dispatch }) => (
   <div>
@@ -11,6 +11,17 @@ const ExpenseListFilters = ({ filters, dispatch }) => (
         dispatch(setTextFilter(e.target.value));
       }}
     />
+    <select
+      value={filters.sortBy}
+      onChange={e => {
+        e.target.value === "date"
+          ? dispatch(sortByDate())
+          : dispatch(sortByAmount());
+      }}
+    >
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
   </div>
 );
 
