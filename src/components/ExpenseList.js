9dad57abcbbbcccac7aa79a7a6094785efb0d9db
Samse <samuelberisha@gmail.com>
2019-11-09 10:14:31 +0100

    102. Rendering Individual Expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,15 +1,21 @@
 import React from "react";
+import ExpenseListItem from "./ExpenseListItem";
 import { connect } from "react-redux";
+import selectExpenses from "../selectors/expenses";
 
-const ExpenseList = props => (
+const ExpenseList = ({ expenses }) => (
   <div>
     <h1>ExpenseList</h1>
-    {props.expenses.length}
+    {expenses.map(expense => (
+      <ExpenseListItem key={expense.id} {...expense} />
+    ))}
   </div>
 );
 
 const mapStateToProps = state => {
-  return { expenses: state.expenses };
+  return {
+    expenses: selectExpenses(state.expenses, state.filters)
+  };
 };
 
 export default connect(mapStateToProps)(ExpenseList);
