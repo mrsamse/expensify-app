3bf74f81afac94ace66265359711dae66fe61004
Samse <samuelberisha@gmail.com>
2020-01-06 15:39:16 +0100

    Added firebase connection

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBUuQmEruEzbqchH_5NmyBg58qWN2pOOM",
+  authDomain: "expensify-app-e2ff0.firebaseapp.com",
+  databaseURL: "https://expensify-app-e2ff0.firebaseio.com",
+  projectId: "expensify-app-e2ff0",
+  storageBucket: "expensify-app-e2ff0.appspot.com",
+  messagingSenderId: "949186587230",
+  appId: "1:949186587230:web:492b6b3057634131e662cf"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase
+  .database()
+  .ref()
+  .set({
+    name: "test"
+  });
